a532cabdb51b095d1f3077393610f78343b5356b
anower52 <anower.hasan@gmail.com>
2023-02-23 16:53:29 +0600

    index page header modified.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,41 +3,39 @@
 @section('meta_description', "add")
 @section('meta_keywords',"add")
 @section('content')
-    <section class="hero-wrapper hero-bg-4 pb-0">
-        <div class="overlay opacity-9"></div><!-- end overlay -->
+    <!-- ================================
+    START HERO-WRAPPER AREA
+================================= -->
+    <section class="hero-wrapper hero-bg-2 pb-0 overflow-hidden">
+        <div class="overlay"></div><!-- end overlay -->
+        <span class="line-bg line-bg1"></span>
+        <span class="line-bg line-bg2"></span>
+        <span class="line-bg line-bg3"></span>
+        <span class="line-bg line-bg4"></span>
+        <span class="line-bg line-bg5"></span>
+        <span class="line-bg line-bg6"></span>
         <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-6">
-                    <div class="hero-heading">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="hero-heading text-center">
                         <div class="section-heading">
-                            <h2 class="sec__title mb-3 line-height-60">Listhub helps you find out whats happening in
-                                your
-                                city, Let's explore.</h2>
-                            <p class="sec__desc line-height-35">
-                                Simple, Yet powerful local business directory where buyers and sellers engage
-                                each others with trust.
-                            </p>
-                        </div>
-                        <div class="btn-box padding-top-35px">
-                            <a href="about.html" class="theme-btn gradient-btn shadow-none">Know More<i
-                                    class="la la-arrow-right ml-2"></i></a>
+                            <h2 class="sec__title">Find The Best Places in your city</h2>
+                            <p class="sec__desc">Listhub  helps you find out whats happening in your city, Let's explore.</p>
                         </div>
                     </div><!-- end hero-heading -->
-                </div><!-- end col-lg-6 -->
-                <div class="col-lg-5 ml-auto">
-                    <div class="main-search-input flex-column mt-0">
-                        <div class="main-search-input-item w-100">
+                    <div class="main-search-input main-search-input-2 position-relative z-index-2">
+                        <div class="main-search-input-item">
                             <form action="#" class="form-box">
                                 <div class="input-box">
                                     <label class="label-text">What are you</label>
-                                    <div class="form-group">
+                                    <div class="form-group mb-0">
                                         <span class="la la-search form-icon"></span>
                                         <input class="form-control" type="search" placeholder="looking for?">
                                     </div>
                                 </div>
                             </form>
                         </div><!-- end main-search-input-item -->
-                        <div class="main-search-input-item user-chosen-select-container w-100 mb-3 ml-0">
+                        <div class="main-search-input-item user-chosen-select-container">
                             <label class="label-text">Where to look?</label>
                             <select class="user-chosen-select">
                                 <option value="0">Select a Location</option>
@@ -46,66 +44,55 @@
                                 <option value="AL">Albania</option>
                             </select>
                         </div><!-- end main-search-input-item -->
-                        <div class="main-search-input-item user-chosen-select-container w-100 mb-3 ml-0">
+                        <div class="main-search-input-item user-chosen-select-container">
                             <label class="label-text">What Category?</label>
                             <select class="user-chosen-select">
                                 <option value="0">Select a Category</option>
                                 <option value="1">Shops</option>
                                 <option value="2">Hotels</option>
+                                <option value="3">Foods & Restaurants</option>
                             </select>
                         </div><!-- end main-search-input-item -->
-                        <div class="main-search-input-item w-100 ml-0">
+                        <div class="main-search-input-item">
                             <label class="label-text">Search Activities</label>
-                            <button class="theme-btn gradient-btn border-0 w-100" type="submit"><i
-                                    class="la la-search mr-2"></i>Search Now
-                            </button>
+                            <button class="theme-btn gradient-btn border-0 w-100" type="submit"><i class="la la-search mr-2"></i>Search Now</button>
                         </div><!-- end main-search-input-item -->
                     </div><!-- end main-search-input -->
-                </div><!-- end col-lg-6 -->
+                </div><!-- end col-lg-12 -->
             </div><!-- end row -->
         </div><!-- end container -->
-        <div class="highlighted-categories bg-dark-opacity margin-top-130px">
-            <div class="highlight-lists d-flex justify-content-center align-items-center">
-                <h5 class="highlighted__title pr-5">Browse by Category <i class="la la-arrow-right ml-2"></i></h5>
-                <div class="hero-category-item rounded-0">
-                    <a href="#" class="d-block hero-cat-link rounded-0">
-                        <span class="icon-element bg-1 mx-auto"><i class="la la-building-o"></i></span>
-                        apartments
-                    </a>
-                </div>
-                <div class="hero-category-item rounded-0">
-                    <a href="#" class="d-block hero-cat-link rounded-0">
-                        <span class="icon-element bg-2 mx-auto"><i class="la la-cutlery"></i></span>
-                        Restaurants
-                    </a>
-                </div>
-                <div class="hero-category-item rounded-0">
-                    <a href="#" class="d-block hero-cat-link rounded-0">
-                        <span class="icon-element bg-3 mx-auto"><i class="la la-plane"></i></span>
-                        travel
-                    </a>
-                </div>
-                <div class="hero-category-item rounded-0">
-                    <a href="#" class="d-block hero-cat-link rounded-0">
-                        <span class="icon-element bg-4 mx-auto"><i class="la la-music"></i></span>
-                        Events
-                    </a>
-                </div>
-                <div class="hero-category-item rounded-0">
-                    <a href="#" class="d-block hero-cat-link rounded-0">
-                        <span class="icon-element bg-5 mx-auto"><i class="la la-gear"></i></span>
-                        fitness
-                    </a>
-                </div>
-                <div class="hero-category-item rounded-0">
-                    <a href="#" class="d-block hero-cat-link rounded-0">
-                        <span class="icon-element bg-6 mx-auto"><i class="la la-hotel"></i></span>
-                        Hotels
-                    </a>
+        <div class="bg-white position-relative z-index-1 pb-4">
+            <div class="container">
+                <div class="row">
+                    <div class="highlighted-categories highlighted-categories-2 pb-5 mx-auto mt-0 padding-top-130px">
+                        <div class="highlight-lists d-flex justify-content-center mt-4">
+                            <div class="hero-category-item hero-category--item">
+                                <a href="#" class="d-block hero-cat-link hover-y">
+                                    <span class="icon-element bg-2 mx-auto"><i class="la la-cutlery"></i></span>
+                                    Restaurants
+                                </a>
+                            </div>
+                            <div class="hero-category-item hero-category--item">
+                                <a href="#" class="d-block hero-cat-link hover-y">
+                                    <span class="icon-element bg-1 mx-auto"><i class="fas fa-dog"></i></span>
+                                    <span class="">Dog Grooming</span>
+                                </a>
+                            </div>
+                            <div class="hero-category-item hero-category--item">
+                                <a href="#" class="d-block hero-cat-link hover-y">
+                                    <span class="icon-element bg-3 mx-auto"><i class="la la-plane"></i></span>
+                                    Travel
+                                </a>
+                            </div>
+                        </div>
+                    </div><!-- end highlighted-categories -->
                 </div>
             </div>
         </div>
-    </section>
+    </section><!-- end hero-wrapper -->
+    <!-- ================================
+        END HERO-WRAPPER AREA
+    ================================= -->
 
     <!-- ====START CATEGORY AREA==== -->
     <section class="category-area bg-gray arrow-down-shape position-relative section-padding">
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -40,6 +40,9 @@
     <link rel="stylesheet" href="{{asset('/css/chosen.min.css')}}">
     <link rel="stylesheet" href="{{asset('/css/style.css')}}">
     <link rel="stylesheet" href="{{asset('/css/custom.css')}}">
+
+{{--    font awesome cdn--}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @yield('css')
 </head>
 <body>
