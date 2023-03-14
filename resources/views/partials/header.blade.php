e35c34ec043ad7f898bf9b0b5808691e904689fb
anower52 <anower.hasan@gmail.com>
2023-03-14 12:00:38 +0600

    cdn removed from header page.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -70,9 +70,7 @@
 </header>
 
 @section('js')
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script
-        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
     <script type="text/javascript">
         var path = "{{ route('autocomplete.search') }}";
 
