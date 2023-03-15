5c46b79928494063c0ae121f6fef21b35adf2128
anower52 <anower.hasan@gmail.com>
2023-03-15 11:33:19 +0600

    laravel avatar installed for showing user profile image.

--- database/migrations/2023_02_15_110441_create_reviews_table.php
+++ database/migrations/2023_02_15_110441_create_reviews_table.php
@@ -20,14 +20,11 @@ class CreateReviewsTable extends Migration
             $table->string('review_id')->nullable();
             $table->string('reviewer_name')->nullable();
             $table->string('reviewer_email')->nullable();
-            $table->text('reviewer_profile_link')->nullable();
             $table->string('reviewer_reviews_count')->nullable();
             $table->string('review_date')->nullable();
             $table->string('review_rate_stars')->nullable();
             $table->text('review_text_original')->nullable();
-            $table->text('reviewer_avatar_url')->nullable();
             $table->text('review_photos_files')->nullable();
-            $table->text('review_photos_urls')->nullable();
             $table->string('review_thumbs_up_value')->nullable();
             $table->timestamps();
         });
