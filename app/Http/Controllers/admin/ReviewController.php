9626f3069d26049abc76b42c4a6972ce328a93bb
anower52 <anower.hasan@gmail.com>
2023-03-20 15:16:02 +0600

    Review show business/organization wise in admin panel.

--- app/Http/Controllers/admin/ReviewController.php
+++ app/Http/Controllers/admin/ReviewController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\Organization;
 use App\Models\Review;
-use App\Models\User;
 use Illuminate\Http\Request;
 use DataTables;
 
@@ -14,14 +13,14 @@ class ReviewController extends Controller
     public function reviewBusiness(Request $request)
     {
         if ($request->ajax()) {
-            $data = Organization::orderByDesc('rate_stars')->get();
+            $data = Organization::where('reviews_total_count', '!=', null)->orderByDesc('rate_stars')->get();
             return Datatables::of($data)
                 ->editColumn('organization_name', function ($organization) {
-                    return '<a href="' . route('admin.reviews.business') . '">' . $organization->organization_name . '</a>';
+                    return '<a href="' . route('admin.reviews', $organization->organization_guid) . '">' . $organization->organization_name . '</a>';
                 })
                 ->addIndexColumn()
-                ->addColumn('action', function ($row) {
-                    $btn = '<a href="javascript:void(0)" class="edit btn btn-primary btn-sm">All Reviews</a>';
+                ->addColumn('action', function ($organization) {
+                    $btn = '<a href="' . route('admin.reviews', $organization->organization_guid) . '" class="edit btn btn-primary btn-sm">All Reviews</a>';
                     return $btn;
                 })
                 ->rawColumns(['action', 'organization_name'])
@@ -30,10 +29,18 @@ class ReviewController extends Controller
         return view('admin.reviews.organization');
     }
 
-    public function reviews()
+    public function reviews(Request $request, $slug)
     {
-        $reviews = Review::latest()->paginate(10);
+        $business = Organization::where('organization_guid', $slug)->first();
 
-        return view('admin.reviews.index', compact('reviews'));
+        if ($request->ajax()) {
+            $data = Review::where('organization_guid', $slug)->get();
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->rawColumns(['action', 'organization_name'])
+                ->make(true);
+        }
+
+        return view('admin.reviews.index',compact('business'));
     }
 }
--- resources/views/admin/reviews/index.blade.php
+++ resources/views/admin/reviews/index.blade.php
@@ -1,13 +1,14 @@
 @extends('admin.master_admin')
-@section('title', 'Organizations')
+@section('title', 'Business')
 @section('css')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"/>
+    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
 @endsection
 @section('content')
     <div class="container-fluid dashboard-inner-body-container">
         <div class="breadcrumb-content d-sm-flex align-items-center justify-content-between mb-4">
             <div class="section-heading">
-                <h2 class="sec__title font-size-24 mb-0">Howdy, {{auth()->user()->name ?? "Guest"}}</h2>
+                <h2 class="sec__title font-size-24 mb-0">Howdy, {{ auth()->user()->name ?? 'Guest' }}</h2>
             </div>
             <ul class="list-items bread-list bread-list-2">
                 <li><a href="{{route('admin.dashboard')}}">Home</a></li>
@@ -19,42 +20,49 @@
                 <div class="block-card dashboard-card mb-4 px-0">
                     <div
                         class="block-card-header d-flex flex-wrap align-items-center justify-content-between px-4 border-bottom-0 pb-0">
-                        <h2 class="widget-title pb-0">All Reviews</h2>
+                        <h2 class="widget-title pb-0">Reviews -> {{ $business->organization_name }}</h2>
                     </div>
                     <div class="block-card-body">
                         <div class="my-table table-responsive">
-                            <table class="table align-items-center table-flush mb-0" id="category_table">
+                            <table class="table table-bordered data_table" id="organization_table">
                                 <thead class="thead-light">
                                 <tr>
                                     <th>No</th>
-                                    <th>Business Name</th>
                                     <th>Reviewer Name</th>
+                                    <th>Review</th>
+                                    <th>Rate Stars</th>
                                     <th>Review Date</th>
-                                    <th>Review Rate Stars</th>
-                                    <th>Review Thumbs</th>
                                 </tr>
                                 </thead>
                                 <tbody>
-                                @foreach($reviews as $key => $review)
-                                    <tr>
-                                        <td>{{++$key}}</td>
-                                        <td>{{Str::title($review->organization->organization_name)}}</td>
-                                        <td>{{Str::title($review->reviewer_name)}}</td>
-                                        <td>{{$review->review_date}}</td>
-                                        <td>{{$review->review_rate_stars}}</td>
-                                        <td>{{$review->review_thumbs_up_value}}</td>
-                                    </tr>
-                                @endforeach
                                 </tbody>
                             </table>
                         </div>
                     </div><!-- end block-card-body -->
-                    {{$reviews->links()}}
                 </div><!-- end block-card -->
             </div><!-- end col-lg-7 -->
         </div><!-- end row -->
     </div><!-- end dashboard-inner-body-container -->
 @endsection
 @section('js')
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+    <script type="text/javascript">
+        $(function () {
+            $.fn.dataTable.ext.errMode = 'throw';
+            let table = $('.data_table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('admin.reviews', $business->organization_guid) }}",
+                columns: [
+                    {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+                    {data: 'reviewer_name', name: 'reviewer_name'},
+                    {data: 'review_text_original', name: 'review_text_original'},
+                    {data: 'review_rate_stars', name: 'review_rate_stars'},
+                    {data: 'review_date', name: 'review_date'}
+                ]
+            });
+        });
+    </script>
 @endsection
