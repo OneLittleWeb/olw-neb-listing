8c6e5a9c44c7c060b52fd97a8f98d461d5b199b0
anower52 <anower.hasan@gmail.com>
2023-03-27 14:10:38 +0600

    Loader removed from show page.

--- resources/views/organization/show.blade.php
+++ resources/views/organization/show.blade.php
@@ -8,15 +8,6 @@
 @section('meta_description', "add")
 @section('meta_keywords',"add")
 @section('content')
-
-    <!-- ===LOADER===== -->
-{{--    <div class="loader-container">--}}
-{{--        <div class="loader-ripple">--}}
-{{--            <div></div>--}}
-{{--            <div></div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-    <!-- ===END LOADER===== -->
     <!-- =======START FULL SCREEN SLIDER======= -->
     <section class="full-screen-slider-area" style="padding-top: 98px">
         @if($organization->organization_photos_files)
