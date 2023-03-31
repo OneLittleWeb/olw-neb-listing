d0c140c68594abcb354979fff2d73693204b1878
Khabirul Basar <kbtonmoy641@gmail.com>
2023-03-31 17:21:46 +0600

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
         $company_joined = Organization::select('organization_name')->distinct()->get();
 
 
-        $posts = Post::published()->taxonomy('category', '	Uncategorized')->take(6)->get();
+        $posts = Post::taxonomy('category', 'uncategorized')->newest()->take(6)->get();
 
 
         dd($posts);
