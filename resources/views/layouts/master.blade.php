b73021b1d9547df84ed3df6db48844834e725f61
Khabirul Basar <kbtonmoy641@gmail.com>
2023-04-03 20:35:58 +0600

    G tag and clarity tag added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -45,6 +45,32 @@
           crossorigin="anonymous" referrerpolicy="no-referrer"/>
     @yield('css')
     @yield('json-ld')
+    @production
+        <!-- Clarity tracking -->
+        <script>
+            (function (c, l, a, r, i, t, y) {
+                c[a] = c[a] || function () {
+                    (c[a].q = c[a].q || []).push(arguments)
+                };
+                t = l.createElement(r);
+                t.async = 1;
+                t.src = "https://www.clarity.ms/tag/" + i + "?ref=bwt";
+                y = l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t, y);
+            })(window, document, "clarity", "script", "gisv7cfnth");
+        </script>
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T89B1MD5QN"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', 'G-T89B1MD5QN');
+        </script>
+    @endproduction
 </head>
 <body>
 
