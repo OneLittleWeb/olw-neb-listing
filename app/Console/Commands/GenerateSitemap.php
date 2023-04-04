2b8341949496bdbad666467f912fe2fbe45b5842
Khabirul Basar <kbtonmoy641@gmail.com>
2023-04-05 02:36:21 +0600

    Sitemap generation notification added

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -3,7 +3,11 @@
 namespace App\Console\Commands;
 
 use App\Http\Controllers\SitemapController;
+use App\Notifications\SitemapUpdateNotification;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Notification;
 
 class GenerateSitemap extends Command
 {
@@ -38,7 +42,25 @@ class GenerateSitemap extends Command
      */
     public function handle()
     {
-        $sitemap = new SitemapController();
-        $sitemap->sitemapStore();
+       try {
+            File::ensureDirectoryExists(public_path('old_sitemaps'));
+            $path = public_path('old_sitemaps');
+            $files = File::cleanDirectory($path);
+            $searchFiles = File::glob(public_path() . '/' . 'sitemap*');
+            foreach ($searchFiles as $f) {
+                $filename = substr($f, strrpos($f, '/') + 1);
+                File::move(public_path() . '/' . $filename, public_path('old_sitemaps') . '/' . $filename);
+            }
+            $sitemap = new SitemapController();
+            $sitemap->sitemapStore();
+            $message = 'Hey, NebraskaListing sitemap is updated on ' . Carbon::now()->format('d m Y.');
+            Notification::send('nothing', new  SitemapUpdateNotification($message));
+        } catch (\Exception $e) {
+            $message = 'Hey, There is an issue please check ';
+            Notification::send('nothing', new  SitemapUpdateNotification($message));
+        } catch (\Error $e){
+            $message = 'Hey, There is an error: '.$e->getMessage();
+            Notification::send('nothing', new  SitemapUpdateNotification($message));
+        }
     }
 }
---
+++ app/Notifications/SitemapUpdateNotification.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\Telegram\TelegramMessage;
+
+class SitemapUpdateNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+         return ["telegram"];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toTelegram($notifiable)
+    {
+        return TelegramMessage::create()
+          ->to('-832753642')
+          ->content($this->message);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
