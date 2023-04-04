918e56da05a9209fdf0076f14db1dd49c1fec532
Khabirul Basar <kbtonmoy641@gmail.com>
2023-04-05 02:11:38 +0600

    sitemap generation command added

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\SitemapController;
+use Illuminate\Console\Command;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:sitemap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This will generate sitemap files';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $sitemap = new SitemapController();
+        $sitemap->sitemapStore();
+    }
+}
