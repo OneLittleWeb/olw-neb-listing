4ee22ad89331cac5ce35db96e7f8c38a93272073
anower52 <anower.hasan@gmail.com>
2023-08-16 13:59:51 +0600

    suggest and edit details modal added.

--- resources/views/admin/organization/suggest_edit_request.blade.php
+++ resources/views/admin/organization/suggest_edit_request.blade.php
@@ -102,7 +102,9 @@
 
                                             <div class="d-inline">
                                                 <button type="submit" class="btn btn-sm btn-info d-inline"
-                                                        data-toggle="tooltip" data-placement="top" title="Details">
+                                                        data-toggle="modal"
+                                                        title="Details"
+                                                        onclick="suggestEditDetailsModal({{ $edit_request }})">
                                                     <i class="fa fa-eye" aria-hidden="true"></i></button>
                                             </div>
                                         </td>
@@ -116,8 +118,118 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade suggest-edit-details-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Suggest An Edit Details</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-12">
+
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Old</th>
+                                        <th scope="col">Suggested</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td><span class="font-weight-bold">Business Name:</span></td>
+                                        <td><span id="old_organization_name"></span></td>
+                                        <td><span id="suggested_organization_name"></span></td>
+                                    </tr>
+                                    <tr>
+                                        <td><span class="font-weight-bold">Is It Closed:</span></td>
+                                        <td><span>No</span></td>
+                                        <td><span id="is_it_closed"></span></td>
+                                    </tr>
+                                    <tr>
+                                        <td><span class="font-weight-bold">Temporarily Closed:</span></td>
+                                        <td><span>No</span></td>
+                                        <td><span id="temporarily_closed"></span></td>
+                                    </tr>
+                                    <tr>
+                                        <td><span class="font-weight-bold">Owner:</span></td>
+                                        <td><span>Default</span></td>
+                                        <td><span id="are_you_the_owner"></span></td>
+                                    </tr>
+                                    <tr>
+                                        <td><span class="font-weight-bold">Address:</span></td>
+                                        <td><span id="old_organization_address"></span></td>
+                                        <td><span id="organization_address"></span></td>
+                                    </tr>
+                                    <tr>
+                                        <td><span class="font-weight-bold">Phone Number:</span></td>
+                                        <td><span id="old_organization_phone_number"></span></td>
+                                        <td><span id="organization_phone_number"></span></td>
+                                    </tr>
+                                    <tr>
+                                        <td><span class="font-weight-bold">Organization Website:</span></td>
+                                        <td><span id="old_organization_website"></span></td>
+                                        <td><span id="organization_website"></span></td>
+                                    </tr>
+                                    <tr>
+                                        <td><span class="font-weight-bold">Price List URL:</span></td>
+                                        <td><span></span></td>
+                                        <td><span id="price_list_url"></span></td>
+                                    </tr>
+                                    <tr>
+                                        <td><span class="font-weight-bold">Organization Work Time:</span></td>
+                                        <td><span id="old_organization_work_time"></span></td>
+                                        <td><span id="organization_work_time"></span></td>
+                                    </tr>
+                                    <tr>
+                                        <td><span class="font-weight-bold">Message:</span></td>
+                                        <td><span></span></td>
+                                        <td><span id="message"></span></td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @section('js')
+
+    <script>
+        function suggestEditDetailsModal(edit_request) {
+            $('.suggest-edit-details-modal').modal('show');
+            // document.getElementById('is_it_closed').innerHTML = edit_request.temporarily_closed ? 'Yes' : 'No';
+            $('#old_organization_name').text(edit_request.organization.organization_name);
+            $('#suggested_organization_name').text(edit_request.organization_name);
+            $('#is_it_closed').text(edit_request.is_it_closed ? 'Yes' : 'No');
+            $('#temporarily_closed').text(edit_request.temporarily_closed ? 'Yes' : 'No');
+            $('#are_you_the_owner').text(edit_request.are_you_the_owner ? 'Yes' : 'No');
+            $('#old_organization_address').text(edit_request.organization.organization_address);
+            $('#organization_address').text(edit_request.organization_address);
+            $('#old_organization_phone_number').text(edit_request.organization.organization_phone_number);
+            $('#organization_phone_number').text(edit_request.organization_phone_number);
+            $('#old_organization_website').text(edit_request.organization.organization_website);
+            $('#organization_website').text(edit_request.organization_website);
+            $('#price_list_url').text(edit_request.price_list_url);
+            $('#old_organization_work_time').text(edit_request.organization.organization_work_time);
+            $('#organization_work_time').text(edit_request.organization_work_time);
+            $('#message').text(edit_request.message);
+        }
+    </script>
+
     <script>
         $(document).on('click', 'button.approve-suggest-request', function () {
             Swal.fire({
