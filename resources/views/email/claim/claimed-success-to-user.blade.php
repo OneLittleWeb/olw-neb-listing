b8d3ed09aa2d0f7af48566eb4705556d117bfdf8
anower52 <anower.hasan@gmail.com>
2023-08-22 15:28:13 +0600

    claimed succes to user mail blade modified.

--- resources/views/email/claim/claimed-success-to-user.blade.php
+++ resources/views/email/claim/claimed-success-to-user.blade.php
@@ -169,14 +169,12 @@
                     </td>
                 </tr>
 
-                <!-- start copy -->
                 <tr>
                     <td align="left" bgcolor="#ffffff"
                         style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
                         <p style="margin: 0;">Cheers,<br> {{ env('APP_NAME') }}</p>
                     </td>
                 </tr>
-                <!-- end copy -->
 
             </table>
             <!--[if (gte mso 9)|(IE)]>
@@ -186,10 +184,8 @@
             <![endif]-->
         </td>
     </tr>
-    <!-- end copy block -->
 
 </table>
-<!-- end body -->
 
 </body>
 </html>
